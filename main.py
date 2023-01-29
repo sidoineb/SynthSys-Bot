841dd54d2d36f0528786de6ca656723e33f07cb5
SidoineB <sidoineb@gmail.com>
2023-01-29 20:43:11 +0100

    add .env & message bjr

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('Ici mettre le Token')
+TOKEN = os.getenv("TOKEN")
 
 client = discord.Client()
 
 @client.event
-async def on_ready():
+async def on_message(message):
     print(f'{client.user} est connecté sur le Discord!')
+
+    if message.content.startswith('!bonjour'):
+        await message.channel.send('Bonjour {client.user}!')
 
 client.run(TOKEN)
