72c4d3fbb90e111e673a5541cd497add7f2a5c32
SidoineB <sidoineb@gmail.com>
2023-02-03 20:45:05 +0100

    Add Ban/Unban

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ TOKEN = os.getenv("TOKEN")
 
 client = discord.Client()
 
+# Accueil
+
 @client.event
 async def on_message(message):
     print(f'{client.user} est connecté sur le Discord!')
@@ -15,14 +17,39 @@ async def on_message(message):
     if message.content.startswith('!bonjour'):
         await message.channel.send('Bonjour {client.user}!')
 
+
+# Kick
+
 @client.command()
-async def ban(ctx, user:discord.User, reason):
+async def kick(ctx, user:discord.User, reason):
     if ctx.author.guild_permissions.kick_members:
         await ctx.guild.kick(user)
         await ctx.respond(f'Kick de {user} - Raison: (reason)')
 
     else:
         await ctx.respond("Vous n'avez pas les droits pour Kick un membre!")
+
+# Ban
+
+@client.command()
+async def ban(ctx, user:discord.User, reason):
+    if ctx.author.guild_permissions.ban_members:
+        await ctx.guild.ban(user)
+        await ctx.respond(f'ban de {user} - Raison: (reason)')
+
+    else:
+        await ctx.respond("Vous n'avez pas les droits pour Ban un membre!")
+
+# Unban
+
+@client.command()
+async def unban(ctx, user:discord.User, reason):
+    if ctx.author.guild_permissions.ban_members:
+        await ctx.guild.unban(user)
+        await ctx.respond(f'Unban de {user} - Raison: (reason)')
+
+    else:
+        await ctx.respond("Vous n'avez pas les droits pour Unban un membre!")
 
 
 client.run(TOKEN)
