4989e53cf396203532f35a18b0f532b6a3ac95a8
SidoineB <sidoineb@gmail.com>
2023-02-08 23:20:11 +0100

    clean code

--- main.py
+++ main.py
@@ -23,36 +23,43 @@ async def on_message(message):
 @client.command()
 async def kick(ctx, user: discord.User, reason: str):
 
-    if ctx.author.guild_permissions.kick_members:
-        await ctx.guild.await ctx.guild.kick(user, reason=reason)
-        await ctx.await ctx.send(f"Utilisateur kické: {user.name} ({user.id}) - Raison: {reason}")
-
-    else:
+    if ctx.author.guild_permissions.kick_members is True:
+        if reason:
+            await ctx.guild.kick(user, reason=reason)
+            await ctx.send(f"Utilisateur kické: {user.name} ({user.id}) - Raison: {reason}")
+        else:
+            await ctx.send("Veuillez entrer une raison pour le kick.")
+        else:
         await ctx.send("Vous n'avez pas la permission de bannir un utilisateur.")
+
 
 # Ban
 
 @client.command()
 async def ban(ctx, user: discord.User, reason: str):
 
-    if ctx.author.guild_permissions.ban_members:
-        await ctx.guild.await ctx.guild.ban(user, reason=reason)
-        await ctx.await ctx.send(f"Utilisateur banni: {user.name} ({user.id}) - Raison: {reason}")
-
-    else:
+    if ctx.author.guild_permissions.ban_members is True:
+        if reason:
+            await ctx.guild.ban(user, reason=reason)
+            await ctx.send(f"Utilisateur banni: {user.name} ({user.id}) - Raison: {reason}")
+        else:
+            await ctx.send("Veuillez entrer une raison pour le bannir.")
+        else:
         await ctx.send("Vous n'avez pas la permission de bannir un utilisateur.")
+
 
 # Unban
 
 @client.command()
 async def unban(ctx, user: discord.User, reason: str):
 
-    if ctx.author.guild_permissions.ban_members:
-        await ctx.guild.await ctx.guild.unban(user, reason=reason)
-        await ctx.await ctx.send(f"Utilisateur unbanni: {user.name} ({user.id}) - Raison: {reason}")
-
-    else:
+    if ctx.author.guild_permissions.ban_members is True:
+        if reason:
+            await ctx.guild.unban(user, reason=reason)
+            await ctx.send(f"Utilisateur unbanni: {user.name} ({user.id}) - Raison: {reason}")
+        else:
+            await ctx.send("Veuillez entrer une raison pour le unbannir.")
+        else:
         await ctx.send("Vous n'avez pas la permission de unbannir un utilisateur.")
-
 
 client.run(TOKEN)
